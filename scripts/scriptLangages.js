dcf2a7681f7cd6f17bd2f0d475e7a06a7ff89946
Loric Audin <loricaudin@yahoo.fr>
2024-04-20 20:40:40 +0200

    V0.5.4 Avancement mémento HTML et CSS et ajout mémento JS

--- scripts/scriptLangages.js
+++ scripts/scriptLangages.js
@@ -27,7 +27,7 @@ let listeLangages = [
     ["Python", "python", "python", "rgba(255, 212, 65, .5)", ""],
     ["HTML", "html", "html", "rgba(229, 76, 33, .5)", ""],
     ["CSS", "css", "css", "rgba(26, 77, 228, .5)", ""],
-    //["JavaScript", "js", "js", "rgba(247, 223, 30, .5)", ""],
+    ["JavaScript", "js", "js", "rgba(247, 223, 30, .5)", ""],
     //["PHP", "php", "php", "", ""],
     ["MySQL", "sql", "mysql", "rgba(228, 142, 0, .5)", ""],
     ["Oracle Database (SQL)", "sql", "oracledatabase", "rgba(250, 100, 100, .5)", ""],
