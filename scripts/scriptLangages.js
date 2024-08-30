73e20a5db9ad4b55ecfcd2da37dbe4764a4f3e23
Loric Audin <loricaudin@yahoo.fr>
2024-08-30 19:25:45 +0200

    V1.7.3 Correction mémento et retrait provisoire PHP

--- scripts/scriptLangages.js
+++ scripts/scriptLangages.js
@@ -1,4 +1,29 @@
 // Liste des langages avec le lien du fichier et une couleur choisie pour le mode clair et sombre.
+let listeLangages = [
+    ["Python", "python", "python", "rgba(255, 212, 65, .5)", ""],
+    ["HTML", "html", "html", "rgba(229, 76, 33, .5)", ""],
+    ["CSS", "css", "css", "rgba(26, 77, 228, .5)", ""],
+    ["JavaScript", "js", "js", "rgba(247, 223, 30, .5)", ""],
+    //["PHP", "php", "php", "rgba(122, 134, 184, .5)", ""],
+    ["MySQL", "sql", "mysql", "rgba(228, 142, 0, .5)", ""],
+    ["Oracle Database (SQL)", "sql", "oracledatabase", "rgba(250, 100, 100, .5)", ""],
+    //["SQL Server", "sql", "sqlserver", "", ""],
+    //["SQLite", "sql", "sqlite", "", ""],
+    ["MongoDB (NoSQL)", "nosql", "mongodb", "rgba(100, 200, 100)", ""],
+    ["C", "c", "c", "rgba(0, 89, 157, .5)", ""],
+    ["C++", "cpp", "cpp", "rgba(0, 68, 130, .5)", ""],
+    //["C#", "cs", "cs", "", ""],
+    ["Java", "java", "java", "rgba(240, 145, 17, .5)", ""],
+    //["Android", "android", "android", "", ""],
+    //["Kotlin", "android", "kotlin", "", ""],
+    //["ReactJS", "js", "reactjs", "", ""]
+    //["VueJS", "js", "vuejs", "", ""]
+    //["ThreeJS", "js", "threejs", "", ""]
+];
+
+
+
+// Script
 
 var langages = document.querySelector(".langages");
 
@@ -22,25 +47,6 @@ function ajouterLangage(nouveaulangage){
         setTimeout(function(){lien.style.background = "none"}, 1000);
     });
 }
-
-let listeLangages = [
-    ["Python", "python", "python", "rgba(255, 212, 65, .5)", ""],
-    ["HTML", "html", "html", "rgba(229, 76, 33, .5)", ""],
-    ["CSS", "css", "css", "rgba(26, 77, 228, .5)", ""],
-    ["JavaScript", "js", "js", "rgba(247, 223, 30, .5)", ""],
-    ["PHP", "php", "php", "rgba(122, 134, 184, .5)", ""],
-    ["MySQL", "sql", "mysql", "rgba(228, 142, 0, .5)", ""],
-    ["Oracle Database (SQL)", "sql", "oracledatabase", "rgba(250, 100, 100, .5)", ""],
-    //["SQLite", "sql", "sqlite", "", ""],
-    ["MongoDB (NoSQL)", "nosql", "mongodb", "rgba(100, 200, 100)", ""],
-    ["C", "c", "c", "rgba(0, 89, 157, .5)", ""],
-    ["C++", "cpp", "cpp", "rgba(0, 68, 130, .5)", ""],
-    //["C#", "cs", "cs", "", ""],
-    ["Java", "java", "java", "rgba(240, 145, 17, .5)", ""],
-    //["Android", "android", "android", "", ""],
-    //["ReactJS", "js", "reactjs", "", ""]
-    //["ReactJS", "js", "vuejs", "", ""]
-];
 
 for(let i = 0; i < listeLangages.length; i++){
     ajouterLangage(listeLangages[i]);
