b65de910ab724c2897a3ef1d7e0b614b5e1c57ab
Loric Audin <loricaudin@yahoo.fr>
2024-10-07 19:29:26 +0200

    V1.8 Transformation partie "Langages" en "Mémentos"

--- mementos/__scripts__/scriptMementos.js
+++ mementos/__scripts__/scriptMementos.js
@@ -1,5 +1,5 @@
-// Liste des langages avec le lien du fichier et une couleur choisie pour le mode clair et sombre.
-let listeLangages = [
+// Liste des mementos avec le lien du fichier et une couleur choisie pour le mode clair et sombre.
+let listeMementos = [
     ["Python", "python", "python", "rgba(255, 214, 66, .5)", "rgba(226, 178, 0, .5)"],
     ["HTML", "html", "html", "rgba(241, 101, 41, .5)", "rgba(228, 77, 38, .5)"],
     ["CSS", "css", "css", "rgba(41, 101, 241, .5)", "rgba(38, 77, 228, .5)"],
@@ -14,41 +14,44 @@ let listeLangages = [
     ["C++", "cpp", "cpp", "rgba(0, 89, 157, .5)", "rgba(0, 68, 129, .5)"],
     //["C#", "cs", "cs", "", ""],
     ["Java", "java", "java", "rgba(245, 131, 25, .5)", "rgba(217, 108, 9, .5)"],
+    //["Spring (et Spring Boot)", "java", "spring", "rgba(102, 178, 41, .5)", "rgba(86, 150, 34, .5)"],
     //["Android", "android", "android", "", ""],
     //["Kotlin", "android", "kotlin", "", ""],
-    //["ReactJS", "js", "reactjs", "", ""]
-    //["VueJS", "js", "vuejs", "", ""]
-    //["ThreeJS", "js", "threejs", "", ""]
-    //["Angular", "js", "angular", "", ""]
+    //["ReactJS", "js", "reactjs", "", ""],
+    //["VueJS", "js", "vuejs", "", ""],
+    //["ThreeJS", "js", "threejs", "", ""],
+    //["Angular", "js", "angular", "", ""],
+    //["Docker", "docker", "docker", "", ""],
+    //["Git", "git", "git", "", ""]
 ];
 
 
 
 // Script
 
-var langages = document.querySelector(".langages");
+var mementos = document.querySelector(".mementos");
 
-function ajouterLangage(nouveaulangage){
-    let langage = document.createElement("div");
-    langage.classList.add("langage");
+function ajouterMemento(nouveauMemento){
+    let memento = document.createElement("div");
+    memento.classList.add("memento");
     let lien = document.createElement("a");
-    lien.setAttribute("href", nouveaulangage[1] + "/" + nouveaulangage[2] + ".html");
-    lien.textContent = nouveaulangage[0];
+    lien.setAttribute("href", nouveauMemento[1] + "/memento_" + nouveauMemento[2] + ".html");
+    lien.textContent = nouveauMemento[0];
     let div = document.createElement("div");
     lien.appendChild(div);
     let image = document.createElement("img");
-    image.setAttribute("src", nouveaulangage[1] + "/icone_" + nouveaulangage[2] + ".png");
+    image.setAttribute("src", nouveauMemento[1] + "/icone_" + nouveauMemento[2] + ".png");
     div.appendChild(image);
 
-    langage.append(lien);
-    langages.append(langage);
+    memento.append(lien);
+    mementos.append(memento);
 
-    langage.addEventListener('mousedown', function(){
-        lien.style.background = nouveaulangage[3];
+    memento.addEventListener('mousedown', function(){
+        lien.style.background = nouveauMemento[3];
         setTimeout(function(){lien.style.background = "none"}, 1000);
     });
 }
 
-for(let i = 0; i < listeLangages.length; i++){
-    ajouterLangage(listeLangages[i]);
+for(let i = 0; i < listeMementos.length; i++){
+    ajouterMemento(listeMementos[i]);
 }
--- scripts/header.js
+++ scripts/header.js
@@ -40,7 +40,7 @@ header.innerHTML = /*html*/
 		    <li><a href="${emplacementRacine + 'index.html'}">Accueil</a></li>
             <li><a href="${emplacementRacine + 'aides/aides.html'}">Aides</a></li>
 		    <li><a href="${emplacementRacine + 'astuces/astuces.html'}">Astuces</a></li>
-		    <li><a href="${emplacementRacine + 'langages/langages.html'}">Langages</a></li>
+		    <li><a href="${emplacementRacine + 'mementos/mementos.html'}">Mémentos</a></li>
 		    <li><a href="${emplacementRacine + 'apropos.html'}">À propos</a></li>
 		    
 		</ul>
